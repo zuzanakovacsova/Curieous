33bab1bbe21509df41847c1c7ea94c09181db539
Zuzana Kovacsova <suz.kovacsova@gmail.com>
2019-11-18 12:42:25 +0000

    Added disclaimer
    - replaced requirements
    - also removed search bar

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ $("#Surprise").click(function (){
      $("#Surprise:hover").css("cursor", "wait");
 }) 
 
+function submitForm() {
+     var frm = document.getElementsByName('contact-form')[0];
+     frm.reset();  // Reset all form data
+     $("#appended_confirm").append("Thank you, your request has been submitted and we will get in touch shortly.");   
+     return false; // Prevent page refresh
+}
